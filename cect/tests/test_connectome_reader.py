fe46fb0d66048a6585fda1fd7e02b207ea6ad678
nanoman657 <nanoman657@gmail.com>
2025-03-22 23:42:01 -0500

    test: validate muscle cell is placed in correct group

--- cect/tests/test_connectome_reader.py
+++ cect/tests/test_connectome_reader.py
@@ -1,5 +1,7 @@
 from cect import print_
 import unittest
+from cect.ConnectomeReader import check_cells
+from cect.Cells import PREFERRED_HERM_NEURON_NAMES
 
 
 class TestConnectomeReader(unittest.TestCase):
@@ -21,6 +23,30 @@ class TestConnectomeReader(unittest.TestCase):
         print_("Reloaded: %s" % str2)
 
         assert str1 == str2
+
+
+class TestCheckCells(unittest.TestCase):
+    def test_should_group_muscle_cells(self):
+        cells_input = ("MDL01",)
+
+        cells_processed = check_cells(cells_input)
+
+        in_preferred_output = cells_processed[0]
+        not_in_preferred_output = cells_processed[1]
+        missing_preferred_output = cells_processed[2]
+        muscles_output = cells_processed[3]
+
+        muscles_output_expected = list(cells_input)
+        assert not in_preferred_output, "Cells included unexpectedly in 'in_preferred'"
+        assert (
+            missing_preferred_output == PREFERRED_HERM_NEURON_NAMES
+        ), "Not all preferred herm neuron names missing"
+        assert (
+            not not_in_preferred_output
+        ), "Cells included unexpectedly in 'not_in_preferred'"
+        assert (
+            muscles_output == muscles_output_expected
+        ), "Input muscle cells are not all grouped together"
 
 
 if __name__ == "__main__":
